98122f13a72b3fe73fd23c876bb3adf600aac19a
ranxiaoxu <18302969902@163.com>
2016-07-11 10:48:27 -0700

    add something

--- my_shell.c
+++ my_shell.c
@@ -9,14 +9,31 @@ int main()
 	fflush(stdout);
 	char * buf[1024];
 	memset(buf,'\0',sizeof(buf));
-	ssize sz = read(0,buf,sizeof(buf));
+	ssize_t sz = read(0,buf,sizeof(buf));
 	if(sz > 0)
 		buf[sz-1] = '\0';
 
+	char *my_argv[64];
+	char *p = buf;
+	int index = 0;
+	my_argv[0] = p;
+	while(*p != '\0')
+	{
+		if(*p == ' ')
+		{
+			*p = '\0';
+			p++;
+			my_argv[index++] = p;
+			continue;
+		}
+		p++;
+	}
+	my_argv[index] = NULL;
+	
 	pid_t id = fork();
 	if(id == 0)   //child
 	{
-		
+			execvp(my_argv[0],my_argv);
 	}
 	else if(id > 0)   //father
 	{
